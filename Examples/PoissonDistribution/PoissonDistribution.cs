12799776d631ad275be4ab593e2344295cab086b
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-01-31 23:19:05 +0100

    Added the poisson distribution. (Probability mass function)

---
+++ Examples/PoissonDistribution/PoissonDistribution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PoissonDistribution
+{
+    public enum PoissonFunctionType
+    {
+        Mass,
+        Cumulative
+    }
+
+    public sealed class PoissonDistribution
+    {
+        private decimal Lambda { get; }
+
+        public PoissonDistribution(decimal lambda)
+        {
+            Lambda = lambda;
+        }
+
+        public decimal Compute(int k, PoissonFunctionType type)
+        {
+            switch (type)
+            {
+                case PoissonFunctionType.Mass:
+                    var initial = k;
+                    var result = 0.0M;
+                    while (initial >= 0)
+                    {
+                        result += MassFunction(initial);
+                        initial--;
+                    }
+                    return result;
+            }
+
+            return 0;
+        }
+
+        // Probability mass function
+        private decimal MassFunction(int k)
+        {
+            var factorial = Factorial(k); // Gets factorial 
+            var a = (Math.Pow((double)Lambda, k)) * (Math.Pow(Math.E, (double)-Lambda));
+            var result = (decimal)a / factorial;
+            return result;
+        }
+
+        // Computes factorial 
+        private static int Factorial(int k)
+        {
+            var initial = k;
+            var factorial = 1;
+
+            while (initial >= 1)
+            {
+                factorial = factorial * initial;
+                initial--;
+            }
+            return factorial;
+        }
+    }
+}
---
+++ Examples/PoissonDistribution/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PoissonDistribution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *
+
+            ----------------------
+            Poisson Distribution:
+            ----------------------
+            Suppose that we can expect some independent event to occur '^' times over 
+            a specified time interval. The probability of exactly 'x' occurances is equal to: https://wikimedia.org/api/rest_v1/media/math/render/svg/c3aa61d2a6d2e549fba969b4cfb923e5f612356a (Formula)
+
+            MORE INFO: https://en.wikipedia.org/wiki/Poisson_distribution
+                          
+             * 
+             */
+
+            /*
+            
+            Suppose a fast food restaurant can expect 2 customers every 3 minutes, on avg. 
+            What is the probability that four or fewer patrons will enter the restaurant in a 9 minute period ?                       
+
+            = 6 customers in 9 minutes (^)
+             
+            => P(0) + P(1) + P(2) + P(3) + P(4)
+             */
+
+            var poissonDistibution = new PoissonDistribution(6);
+            var result = poissonDistibution.Compute(4,PoissonFunctionType.Mass);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
