13c06f31940991eb79816099baeae7565d55d9ac
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-02-01 20:56:18 +0100

    Added cosine similarity

---
+++ Examples/CosineSimilarity/CosineSimilarity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CosineSimilarity
+{
+    public sealed class CosineSimilarity
+    {
+        public double? Compute(double[] vectorA , double[] vectorB)
+        {
+            if (vectorA.Length != vectorB.Length)
+            {
+                return null;
+            }
+
+            var dotProduct = 0.0d;
+            var preMagnitudeA = 0.0d;
+            var preMagnitudeB = 0.0d;
+
+            for (int i = 0 ; i < vectorA.Length ; i++)
+            {
+                var a = vectorA[i];
+                var b = vectorB[i];
+                var mult = a*b;
+                dotProduct += mult;
+
+                var aMag = Math.Pow(a, 2);
+                preMagnitudeA += aMag;
+
+                var bMag = Math.Pow(b, 2);
+                preMagnitudeB += bMag;
+            }
+
+            var prodMagnitude = Math.Sqrt(preMagnitudeA*preMagnitudeB);
+            var result = dotProduct/prodMagnitude;
+            return result;
+        }
+    }
+}
---
+++ Examples/CosineSimilarity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CosineSimilarity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vectorA = new double[] { 5, 0, 3, 0, 2, 0, 0, 2, 0, 0 };
+            var vectorB = new double[] { 3, 0, 2, 0, 1, 1, 0, 1, 0 ,1};
+            var cosineSimilarity = new CosineSimilarity();
+
+            var result = cosineSimilarity.Compute(vectorA, vectorB);
+            Console.WriteLine($"Cosine Similarity : {result} or {result * 100} %");
+
+            Console.ReadKey();
+        }
+    }
+}
